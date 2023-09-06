c5e5d849727d0f1aceaa3bfcd6a90a22c7b9a25e
Lufael <143156042+suthipongch@users.noreply.github.com>
2023-09-06 13:56:17 +0700

    update demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Hexagon, Honeycomb, ResponsiveHoneycomb } from "react-honeycomb";
+
+import logo from "./logo.svg";
+import range from "lodash/range";
+
+const items = range(7);
+const sideLength = 64;
 
 function App() {
+  // const randomColor = Math.floor(Math.random()*16777215).toString(16)
+  // console.log(randomColor)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Honeycomb
+        columns={3}
+        size={sideLength}
+        items={items}
+        renderItem={(item, index) => (
+          <Hexagon>
+            <img
+              src={`https://picsum.photos/${sideLength * 2}?random=${item}`}
+              alt={`Random #${item}`}
+            />
+          </Hexagon>
+        )}
+      />
+
+      <Honeycomb
+        columns={4}
+        size={sideLength}
+        items={items}
+        renderItem={(item, index) => (
+          <Hexagon
+            style={{
+              backgroundColor: `#${Math.floor(
+                Math.random() * 16777215
+              ).toString(16)}`,
+            }}
+          ></Hexagon>
+        )}
+      />
+
+      <ResponsiveHoneycomb
+        defaultWidth={1024}
+        size={sideLength}
+        items={items}
+        renderItem={(item) => (
+          <Hexagon>
+            <img
+              src={`https://picsum.photos/${sideLength * 2}?random=${item}`}
+              alt={`Random #${item}`}
+            />
+          </Hexagon>
+        )}
+      />
     </div>
   );
 }

7c698d52865e11eed0ef028cb85774b6d2e266e2
Lufael <143156042+suthipongch@users.noreply.github.com>
2023-10-02 11:44:20 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,65 @@ import { Hexagon, Honeycomb, ResponsiveHoneycomb } from "react-honeycomb";
 import logo from "./logo.svg";
 import range from "lodash/range";
 
-const items = range(7);
-const sideLength = 64;
+const items = range(9);
+const sideLength = 128;
+const itemsTest = range(2);
+const itemsTest2 = range(3);
 
 function App() {
   // const randomColor = Math.floor(Math.random()*16777215).toString(16)
   // console.log(randomColor)
+  // console.log(items)
   return (
     <div className="App">
+      <Honeycomb
+        columns={2}
+        size={100}
+        items={itemsTest}
+        renderItem={(item, index) => (
+          <>
+            <Hexagon
+              style={{
+                backgroundColor: `#${Math.floor(
+                  Math.random() * 16777215
+                ).toString(16)}`,
+              }}
+            ></Hexagon>
+            <button
+              style={{ zIndex: 10000, position: "relative", top: 0, bottom: 0 }}
+            >
+              test {`#${index}`}{" "}
+            </button>
+          </>
+        )}
+      />
+      <Honeycomb
+        columns={3}
+        size={100}
+        items={itemsTest2}
+        renderItem={(item, index) => (
+          <>
+            <Hexagon
+              style={{
+                backgroundColor: `#${Math.floor(
+                  Math.random() * 16777215
+                ).toString(16)}`,
+              }}
+            ></Hexagon>
+            <button
+              style={{
+                zIndex: 10000,
+                position: "relative",
+                top: 50,
+                bottom: 50,
+              }}
+            >
+              test {`#${index}`}{" "}
+            </button>
+          </>
+        )}
+      />
+
       <Honeycomb
         columns={3}
         size={sideLength}
@@ -28,22 +79,28 @@ function App() {
       />
 
       <Honeycomb
-        columns={4}
+        columns={3}
         size={sideLength}
         items={items}
+        gap={10}
         renderItem={(item, index) => (
           <Hexagon
             style={{
               backgroundColor: `#${Math.floor(
                 Math.random() * 16777215
               ).toString(16)}`,
+              display: index == 2 || index == 8 ? "none" : "block",
             }}
-          ></Hexagon>
+          >
+            <div className="row">
+              
+            </div>
+          </Hexagon>
         )}
       />
 
       <ResponsiveHoneycomb
-        defaultWidth={1024}
+        defaultWidth={1900}
         size={sideLength}
         items={items}
         renderItem={(item) => (
